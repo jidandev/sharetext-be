5f4665f1862548eaa0da5a4d21775c432d5cfe7a
jidandev <jidangamer7@gmail.com>
2025-02-14 23:02:57 +0700

    add endpoint update title, update lang, update text, and delete subtask

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -36,6 +36,16 @@ exports.getSubTasks = (req, res) => {
   })
 }
 
+exports.getSubTask = (req, res) => {
+  let subTaskId = req.params.id;
+  
+  const query = `SELECT * FROM subtasks where id = ?`
+  db.query(query, [subTaskId], (err, results) => {
+    if (err) return res.status(500).json({ error: err.message });
+        res.json(results);
+  })
+}
+
 exports.addTask = (req, res) => {
     const { userId, title } = req.body;
     db.query('INSERT INTO tasks (title,user_id) VALUES (?,?)', [title || "New task", userId], (err, results) => {
@@ -55,6 +65,53 @@ exports.addSubTask = (req, res) => {
         res.json({ message: 'User added successfully', data: {id: results.insertId, task_id: taskId, title: title || "Title", text: text || "", lang: lang || "text"} });
     });
 };
+
+exports.changeTitleSubTask = (req,res) => {
+  const subTaskId = req.params.id;
+  const {title} = req.body;
+  
+  if(!title) {
+   return res.status(200).json({message: "Title tidak diganti"})
+  }
+  
+  const query = `UPDATE subtasks SET title = ? WHERE id = ?`;
+  
+  db.query(query, [title,subTaskId], (err,results) => {
+    if(err) return res.status(500).json({message: "Gagal update data", error: err})
+    
+    res.status(200).json({message: "Subtask title berhasil diperbarui", data: {id: subTaskId, title: title}})
+  })
+}
+
+exports.changeLangSubTask = (req,res) => {
+  const subTaskId = req.params.id;
+  const {lang} = req.body;
+  
+  if(!lang) {
+   return res.status(200).json({message: "Language tidak diganti"})
+  }
+  
+  const query = `UPDATE subtasks SET lang = ? WHERE id = ?`;
+  
+  db.query(query, [lang,subTaskId], (err,results) => {
+    if(err) return res.status(500).json({message: "Gagal update data", error: err})
+    
+    res.status(200).json({message: "Subtask lang berhasil diperbarui", data: {id: subTaskId, lang: lang}})
+  })
+}
+
+exports.changeTextSubTask = (req,res) => {
+  const subTaskId = req.params.id;
+  const {text} = req.body;
+  
+  const query = `UPDATE subtasks SET text = ? WHERE id = ?`;
+  
+  db.query(query, [text,subTaskId], (err,results) => {
+    if(err) return res.status(500).json({message: "Gagal update data", error: err})
+    
+    res.status(200).json({message: "Subtask text berhasil diperbarui", data: {id: subTaskId, text: text}})
+  })
+}
 
 exports.updateSubTask = (req,res) => {
   const subTaskId= req.params.id;
@@ -87,5 +144,17 @@ exports.updateSubTask = (req,res) => {
     if(err) return res.status(500).json({message: "Gagal update data", error: err})
     
     res.status(200).json({message: "Subtask berhasil diperbarui", id: subTaskId, updatedFields: req.body})
+  })
+}
+
+exports.deleteSubTask = (req,res) => {
+  const subTaskId = req.params.id;
+  
+  const query = `DELETE FROM subtasks WHERE id = ?`;
+  
+  db.query(query, [subTaskId], (err,results) => {
+    if(err) return res.status(500).json({message: "Gagal delete data", error: err})
+    
+    res.status(200).json({message: "Subtask berhasil didelete"})
   })
 }
--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -1,13 +1,18 @@
 const express = require('express');
-const { getTasks, addTask, getTasksByUser, addSubTask, getSubTasks, updateSubTask} = require('../controllers/taskController');
+const { getTasks, addTask, getTasksByUser, addSubTask, getSubTasks, getSubTask, updateSubTask, changeTitleSubTask, changeLangSubTask, changeTextSubTask, deleteSubTask} = require('../controllers/taskController');
 
 const router = express.Router();
 
 router.get('/tasks', getTasks);
 router.get('/tasks/user', getTasksByUser);
 router.get('/subtasks', getSubTasks);
+router.get('/subtask/:id', getSubTask);
 router.post('/tasks', addTask);
 router.post('/subtask', addSubTask);
-router.put('/subtasks/:id', updateSubTask);
+router.put('/subtask/:id', updateSubTask);
+router.put('/subtask/title/:id', changeTitleSubTask)
+router.put('/subtask/lang/:id', changeLangSubTask)
+router.put('/subtask/text/:id', changeTextSubTask)
+router.delete('/subtask/:id', deleteSubTask)
 
 module.exports = router;

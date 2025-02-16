ea8a23adc9c776e95768b01087e846b99455ad56
jidandev <jidangamer7@gmail.com>
2025-02-16 19:20:48 +0700

    add some endpoint

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -1,15 +1,26 @@
 const db = require('../config/db');
+const {nanoid} = require('nanoid');
 
+// exports.getTasks = (req, res) => {
+//     let ids = req.query.ids;
+//     if (!ids) return res.status(200).json({ message: "Empty tasks" });
+
+//     ids = Array.isArray(ids) ? ids.map(Number) : ids.split(",").map(Number);
+
+//     const query = `SELECT * FROM tasks WHERE id IN (?)`;
+//     db.query(query, [ids], (err, results) => {
+//         if (err) return res.status(500).json({ message: 'Cannot get tasks', details: err });
+//         res.status(200).json(results);
+//     });
+// };
 exports.getTasks = (req, res) => {
-    let ids = req.query.ids;
-    if (!ids) return res.status(200).json({ message: "Empty tasks" });
+    let guest = req.query.guest;
+    if (!guest) return res.status(200).json({ message: "Empty tasks" });
 
-    ids = Array.isArray(ids) ? ids.map(Number) : ids.split(",").map(Number);
-
-    const query = `SELECT * FROM tasks WHERE id IN (?)`;
-    db.query(query, [ids], (err, results) => {
-        if (err) return res.status(500).json({ error: err.message });
-        res.json(results);
+    const query = `SELECT * FROM tasks WHERE guest = ?`;
+    db.query(query, [guest], (err, results) => {
+        if (err) return res.status(500).json({ message: 'Cannot get tasks', details: err });
+        res.status(200).json(results);
     });
 };
 
@@ -19,8 +30,8 @@ exports.getTasksByUser = (req, res) => {
 
     const query = `SELECT * FROM tasks WHERE user_id = ?`;
     db.query(query, [userId], (err, results) => {
-        if (err) return res.status(500).json({ error: err.message });
-        res.json(results);
+        if (err) return res.status(500).json({ message: 'Cannot get tasks', details: err });
+        res.status(200).json(results);
     });
 };
 
@@ -31,8 +42,8 @@ exports.getSubTasks = (req, res) => {
   
   const query = `SELECT * FROM subtasks where task_id = ?`
   db.query(query, [taskId], (err, results) => {
-    if (err) return res.status(500).json({ error: err.message });
-        res.json(results);
+    if (err) return res.status(500).json({ message: 'Cannot get subtasks', details: err });
+        res.status(200).json(results);
   })
 }
 
@@ -41,28 +52,24 @@ exports.getSubTask = (req, res) => {
   
   const query = `SELECT * FROM subtasks where id = ?`
   db.query(query, [subTaskId], (err, results) => {
-    if (err) return res.status(500).json({ error: err.message });
-        res.json(results);
+    if (err) return res.status(500).json({ message: 'Cannot get subtask', details: err });
+        res.status(200).json(results);
   })
 }
 
 exports.addTask = (req, res) => {
-    const { userId, title } = req.body;
-    db.query('INSERT INTO tasks (title,user_id) VALUES (?,?)', [title || "New task", userId], (err, results) => {
-        if (err) {
-            return res.status(500).json({ error: err.message });
-        }
-        res.json({ message: 'User added successfully', data: {id: results.insertId, user_id: userId || null, title: title || "New task"} });
+    const { userId, title, guest } = req.body;
+    db.query('INSERT INTO tasks (title,user_id,guest) VALUES (?,?,?)', [title || "New task", userId, guest], (err, results) => {
+        if (err) return res.status(500).json({ message: 'Failed to add task', details: err });
+        res.status(200).json({ message: 'Task added successfully', data: {id: results.insertId, user_id: userId || null, guest: guest || null, title: title || "New task"} });
     });
 };
 
 exports.addSubTask = (req, res) => {
     const { title, text, lang, taskId } = req.body;
     db.query('INSERT INTO subtasks (title, text, lang, task_id) VALUES (?,?,?,?)', [title || "Title", text || '', lang || "text", taskId], (err, results) => {
-        if (err) {
-            return res.status(500).json({ error: err.message });
-        }
-        res.json({ message: 'User added successfully', data: {id: results.insertId, task_id: taskId, title: title || "Title", text: text || "", lang: lang || "text"} });
+        if (err) return res.status(500).json({ message: 'Failed to add subtask', details: err });
+        res.json({ message: 'Subtask added successfully', data: {id: results.insertId, task_id: taskId, title: title || "Title", text: text || "", lang: lang || "text"} });
     });
 };
 
@@ -71,15 +78,15 @@ exports.changeTitleSubTask = (req,res) => {
   const {title} = req.body;
   
   if(!title) {
-   return res.status(200).json({message: "Title tidak diganti"})
+   return res.status(200).json({message: "Title not changed"})
   }
   
   const query = `UPDATE subtasks SET title = ? WHERE id = ?`;
   
   db.query(query, [title,subTaskId], (err,results) => {
-    if(err) return res.status(500).json({message: "Gagal update data", error: err})
+    if (err) return res.status(500).json({ message: 'Failed to updated subtask title', details: err });
     
-    res.status(200).json({message: "Subtask title berhasil diperbarui", data: {id: subTaskId, title: title}})
+    res.status(200).json({message: "Subtask title successfully updated", data: {id: subTaskId, title: title}})
   })
 }
 
@@ -88,15 +95,15 @@ exports.changeLangSubTask = (req,res) => {
   const {lang} = req.body;
   
   if(!lang) {
-   return res.status(200).json({message: "Language tidak diganti"})
+   return res.status(200).json({message: "Language not changed"})
   }
   
   const query = `UPDATE subtasks SET lang = ? WHERE id = ?`;
   
   db.query(query, [lang,subTaskId], (err,results) => {
-    if(err) return res.status(500).json({message: "Gagal update data", error: err})
+    if (err) return res.status(500).json({ message: 'Failed to updated subtask lang', details: err });
     
-    res.status(200).json({message: "Subtask lang berhasil diperbarui", data: {id: subTaskId, lang: lang}})
+    res.status(200).json({message: "Subtask lang successfully updated", data: {id: subTaskId, lang: lang}})
   })
 }
 
@@ -107,9 +114,9 @@ exports.changeTextSubTask = (req,res) => {
   const query = `UPDATE subtasks SET text = ? WHERE id = ?`;
   
   db.query(query, [text,subTaskId], (err,results) => {
-    if(err) return res.status(500).json({message: "Gagal update data", error: err})
+    if (err) return res.status(500).json({ message: 'Failed to saved text', details: err });
     
-    res.status(200).json({message: "Subtask text berhasil diperbarui", data: {id: subTaskId, text: text}})
+    res.status(200).json({message: "Subtask text successfully saved", data: {id: subTaskId, text: text}})
   })
 }
 
@@ -153,8 +160,57 @@ exports.deleteSubTask = (req,res) => {
   const query = `DELETE FROM subtasks WHERE id = ?`;
   
   db.query(query, [subTaskId], (err,results) => {
-    if(err) return res.status(500).json({message: "Gagal delete data", error: err})
+    if (err) return res.status(500).json({ message: 'Failed to delete subtask', details: err });
     
-    res.status(200).json({message: "Subtask berhasil didelete"})
+    res.status(200).json({message: "Subtask successfully delete"})
+  })
+}
+
+async function generateGuestName() {
+  return new Promise((resolve, reject) => {
+    function attemptGuest() {
+      const guest = nanoid(10);
+      const query = `SELECT COUNT(*) as count from guests where name = ?`
+      db.query(query,[guest],(err,results) => {
+        if(err) return reject(err);
+        if(results[0].count > 0) {
+          attemptGuest();
+        } else {
+          resolve(guest)
+        }
+      })
+    }
+    attemptGuest();
+  })
+}
+
+exports.addGuest = async (req,res) => {
+  try {
+    const guest = await generateGuestName();
+    const query = `insert into guests (name) values(?)`;
+    db.query(query,[guest],(err,results) => {
+      if(err) return res.status(500).json({message: 'Databases error', details: err})
+      res.status(200).json({name: guest})
+    })
+  } catch(e) {
+    res.status(500).json({message: 'Error generating guest name', details: e})
+  }
+}
+
+exports.yourTask = (req,res) => {
+  const {taskId,guest}= req.query;
+  
+  if(!taskId) return res.status(500).json({message: 'Taskid required'})
+  if(!guest) return res.status(500).json({message: 'Guest required'})
+  
+  const query = `SELECT * FROM tasks where id = ?`
+  db.query(query,[taskId],(err,results) => {
+    if(err) return res.status(500).json({details: err})
+    
+    if(results[0].guest == guest) {
+      res.status(200).json(true)
+    } else {
+      res.status(200).json(false)
+    }
   })
 }
--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { getTasks, addTask, getTasksByUser, addSubTask, getSubTasks, getSubTask, updateSubTask, changeTitleSubTask, changeLangSubTask, changeTextSubTask, deleteSubTask} = require('../controllers/taskController');
+const { getTasks, addTask, getTasksByUser, addSubTask, getSubTasks, getSubTask, updateSubTask, changeTitleSubTask, changeLangSubTask, changeTextSubTask, deleteSubTask,addGuest, yourTask} = require('../controllers/taskController');
 
 const router = express.Router();
 
@@ -7,8 +7,10 @@ router.get('/tasks', getTasks);
 router.get('/tasks/user', getTasksByUser);
 router.get('/subtasks', getSubTasks);
 router.get('/subtask/:id', getSubTask);
+router.get('/checktask', yourTask)
 router.post('/tasks', addTask);
 router.post('/subtask', addSubTask);
+router.post('/addguest', addGuest)
 router.put('/subtask/:id', updateSubTask);
 router.put('/subtask/title/:id', changeTitleSubTask)
 router.put('/subtask/lang/:id', changeLangSubTask)
